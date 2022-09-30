3de585c28bcd4430bd72f701a686d9ccc3649c8d
golunkater <katerinagolunova@gmail.com>
2022-09-30 15:00:20 +0500

    оп

---
+++ Homework1/Homework1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Костер");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Зинаида Гиппиус");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Живые взоры я встречаю...\r\nОгня, огня! Костер готов.\r\nЯ к ближним руки простираю,\r\nЯ жду движенья, знака, слов...\r\n\r\nС какою радостною мукой\r\nВ очах людей ловлю я свет!\r\nНо говорю... и дышит скукой\r\nИх утомительный ответ.\r\n\r\nЯ отступаю, безоружный,\r\nИ длю я праздный разговор,\r\nИ лью я воду на ненужный,\r\nНа мой безогненный костер.\r\n\r\nО, как понять, что это значит?\r\nКого осудим, – их? меня?\r\nДуша обманутая плачет...\r\nКостер готов – и нет меня.");
+            Console.ReadKey();
+        }
+    }
+}
