e04e4e64790dc8dbeb59b4749d1354fa28bb07c5
golunkater <katerinagolunova@gmail.com>
2022-10-13 22:24:38 +0500

    Task 4 is done.

---
+++ Task004/Task004/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task004
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите действительное значение x:");
+            double x = double.Parse(Console.ReadLine());
+            double y = (x*x + 10)/(Math.Sqrt(x*x + 1));
+            Console.WriteLine("f(x) = " + y);
+            Console.ReadKey();
+        }
+    }
+}
