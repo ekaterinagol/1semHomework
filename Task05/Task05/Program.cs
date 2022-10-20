c2a6e06d568b91eb3c2c2842860b643efc840f28
golunkater <katerinagolunova@gmail.com>
2022-10-20 23:08:30 +0500

    Task 5 is done.

---
+++ Task05/Task05/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = sinuss(2,3) + sinuss(5,7) + sinuss(11,13);
+            Console.WriteLine("x = " + x);
+            Console.ReadKey();
+        }
+        static double sinuss(double x,double y)
+        {
+            return Math.Round(((Math.Sin(x)+Math.Sin(y))/(x+y)),3);
+        }
+    }
+}
