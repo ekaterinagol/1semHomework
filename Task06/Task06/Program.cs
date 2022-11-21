f21fe92ddc28fef67fb4aa57de30efa3878fba78
golunkater <katerinagolunova@gmail.com>
2022-11-21 13:00:06 +0500

    ffff

---
+++ Task06/Task06/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите слово:");
+            string s = Console.ReadLine();
+            string newS = s.ToUpper();
+            Console.WriteLine(convert(newS));
+
+
+            Console.ReadKey();
+        }
+        static string convert(string x="A")
+        {
+            return "S";
+        }
+    }
+}
