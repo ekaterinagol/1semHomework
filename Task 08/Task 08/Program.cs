a26fd2cacb97fb98d8340aa82e29467e86d3a9c6
golunkater <katerinagolunova@gmail.com>
2022-11-21 13:15:25 +0500

    task 008

---
+++ Task 08/Task 08/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task_08
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите значение x:");
+            double x = double.Parse(Console.ReadLine());
+            Console.WriteLine("f(x) = " + yFromX(x));
+
+            Console.ReadKey();
+        }
+        static double yFromX(double x)
+        {
+            if (x < -2) { return -2; }
+            else if (x > 2) { return 2; }
+            else { return x; }
+        }
+    }
+}
