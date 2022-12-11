4cf766be55ea51c2165c8622c525d67066860f6a
golunkater <katerinagolunova@gmail.com>
2022-12-11 11:25:34 +0500

    Task 06.2 не работает

---
+++ Task06.2/Task06.2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task06_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "клоун";
+            Console.WriteLine(word,"{0},{3},{1},{2},{4}");
+
+            Console.ReadKey();
+        }
+    }
+}
