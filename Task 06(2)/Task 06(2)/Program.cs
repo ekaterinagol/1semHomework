e90984f1bd7ace155a28dc295d8c3996e2d058e4
golunkater <katerinagolunova@gmail.com>
2022-12-19 13:21:03 +0500

    123

--- Task 06(2)/Task 06(2)/Program.cs
+++ Task 06(2)/Task 06(2)/Program.cs
@@ -13,6 +13,7 @@ namespace Task_06_2_
             string word = "клоун";
             Console.WriteLine($"Из слова {word}, мы сделали слово {word[0]}{word[3]}{word[1]}{word[2]}{word[4]}");
 
+
             Console.ReadKey();
         }
     }
