4257aad0fe419f689ed43162d585e574c6d94b89
golunkater <katerinagolunova@gmail.com>
2022-12-19 13:17:04 +0500

    Task 6(2) is done

---
+++ Task 06(2)/Task 06(2)/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_06_2_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "клоун";
+            Console.WriteLine(word, " {0},{3},{1},{2},{4}");
+            Console.WriteLine($"Из слова {word}, мы сделали слово {word[0]}{word[3]}{word[1]}{word[2]}{word[4]}");
+
+            Console.ReadKey();
+        }
+    }
+
+}
