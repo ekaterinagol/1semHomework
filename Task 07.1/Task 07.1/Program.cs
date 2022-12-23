98e6ab577e06eb2fab6ab54265c0724c4a03d832
golunkater <katerinagolunova@gmail.com>
2022-12-23 20:55:17 +0500

    7.1 is done.

---
+++ Task 07.1/Task 07.1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_07_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CalculateBool());
+
+            Console.ReadKey();
+        }
+
+        static int EnterNumber()
+        {
+            Console.WriteLine(@"Введите число:");
+            int x = int.Parse(Console.ReadLine());
+            return x;
+        }
+
+        static bool CalculateBool()
+        {
+            bool negative;
+            if ((EnterNumber() <= 0) & (EnterNumber() <= 0) & (EnterNumber() <= 0))
+            {
+                negative = true;
+            }
+            else
+            {
+                negative = false;
+            }
+            return negative;
+        }
+    }
+}
