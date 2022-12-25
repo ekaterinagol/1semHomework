89fcd89f2a5943113a7305694bec8b9a1b668ecb
ekaterinagol <103747746+ekaterinagol@users.noreply.github.com>
2022-12-25 19:32:51 +0300

    Update Program.cs

--- Task 07.1/Task 07.1/Program.cs
+++ Task 07.1/Task 07.1/Program.cs
@@ -12,13 +12,6 @@ namespace Task_07_1
             Console.ReadKey();
         }
 
-        static int EnterNumber(string name)
-        {
-            Console.WriteLine($"Введите число {name}:");
-            int x = int.Parse(Console.ReadLine());
-            return x;
-        }
-
         static bool CalculateBool()
         {
             if ((EnterNumber("k") <= 0) & (EnterNumber("m") <= 0) & (EnterNumber("n") <= 0))
@@ -29,6 +22,13 @@ namespace Task_07_1
             {
                 return false;
             }
+        }
+        
+        static int EnterNumber(string name)
+        {
+            Console.WriteLine($"Введите число {name}:");
+            int x = int.Parse(Console.ReadLine());
+            return x;
         }
     }
 }
