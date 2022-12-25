c1aee447c5c1451df31137b5565e8fc64efb8a6f
golunkater <katerinagolunova@gmail.com>
2022-12-25 21:12:47 +0500

    something is wrong 72

---
+++ Task07_2/Task07_2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task07_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.ReadKey();
+        }
+
+        static bool BelongsToArea()
+        {
+            Console.WriteLine("Введите x:");
+            double x = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите y:");
+            double y = double.Parse(Console.ReadLine());
+            if ((y >= -2)&(y<=1,5))
+            {
+                return true;
+            }
+
+        }
+    }
+}
