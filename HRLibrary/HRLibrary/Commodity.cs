27ad50d2cac1a9e512b6e9beb0b28b713b4308ec
golunkater <katerinagolunova@gmail.com>
2023-06-06 15:03:17 +0500

    Task 12 is done.

---
+++ HRLibrary/HRLibrary/Commodity.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRLibrary
+{
+    public class Commodity
+    {
+        public readonly string VendorCode; //string - так как артикул может содежать и числа, и буквы
+        public string Name { get; set; }
+        public double WholesalePricePerItem { get; set; }
+        public double RetailPricePerItem { get; set; }
+        public UnitOfMeasure CommodityType;
+
+        public string Description;
+        public double Amount;
+
+        public Commodity(string code,string name, double firstCost, double secondCost,UnitOfMeasure commodityType) 
+        {
+            VendorCode = code;
+            Name = name; 
+            WholesalePricePerItem = firstCost;
+            RetailPricePerItem = secondCost;
+            CommodityType = commodityType;
+        }
+
+        public string GetInfo()
+        {
+            var commodityType = "";
+            if (CommodityType == UnitOfMeasure.Thing)
+                commodityType = "шт";
+            else if (CommodityType == UnitOfMeasure.Package)
+                commodityType = "у";
+            else if (CommodityType == UnitOfMeasure.Kilogramm)
+                commodityType = "кг";
+            else if (CommodityType == UnitOfMeasure.Ton)
+                commodityType = "т";
+            return $"{VendorCode}. {Name} - {Description}." +
+                $"Оптовая цена = {WholesalePricePerItem} р/{commodityType}. Розничная цена = {RetailPricePerItem} p/{commodityType}." +
+                $"{Amount} {commodityType} на складе.";
+        }
+    }
+//    Поля/свойства(точка означает, что данное свойство следует передавать в конструктор через параметр) :
+//    • aртикул(только для чтения);
+//• наименование; • оптовая цена за единицу товара;
+//• розничная цена за единицу товара;
+//• единица товара(штуки, упаковки, килограммы, тонны — использовать перечисление);
+//    описание;
+//наличие на складе(в ед. товара).
+}
---
+++ HRLibrary/SalesLibrary.UnitTests/CommodityTests.cs
@@ -0,0 +1,41 @@
+using HRLibrary;
+using System.Reflection;
+using System;
+
+namespace SalesLibrary.UnitTests
+{
+    [TestFixture]
+    public class CommodityTests
+    {
+        static Commodity commodity;
+
+        [SetUp]
+        public void Setup()
+        {
+            commodity = new Commodity("01bcca", "Хлеб «Новоречко»", 70, 74, UnitOfMeasure.Package);
+            commodity.Description = "хлеб белый бездрожжевой. 350 гр";
+            commodity.Amount = 120;
+        }
+
+        [Test]
+        public void ConstructorTest()
+        {
+            Assert.That(commodity.VendorCode, Is.EqualTo("01bcca"));
+            Assert.That(commodity.Name, Is.EqualTo("Хлеб «Новоречко»"));
+            Assert.That(commodity.WholesalePricePerItem, Is.EqualTo(70));
+            Assert.That(commodity.RetailPricePerItem, Is.EqualTo(74));
+            Assert.That(commodity.CommodityType, Is.EqualTo(UnitOfMeasure.Package));
+        }
+
+        [Test]
+        public void GetInfo_Commodity_ValuesString()
+        {
+            string expectedInfo = "01bcca. Хлеб «Новоречко» - хлеб белый бездрожжевой. 350 гр."; 
+            expectedInfo += "Оптовая цена = 70 р/у. Розничная цена = 74 p/у."; 
+            expectedInfo += "120 у на складе."; 
+
+            Assert.That(commodity.GetInfo(), Is.EqualTo(expectedInfo));
+        }
+
+    }
+}
