6f7b6ba4e275ba9e664f1d284ecc1a7fbba89437
golunkater <katerinagolunova@gmail.com>
2023-06-22 09:31:36 +0500

    Exam

---
+++ SolitaireHuntForm/SolitaireHuntForm/Program.cs
@@ -0,0 +1,28 @@
+﻿using CardsLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SolitaireHuntForm
+{
+    public static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+
+            
+
+            }
+
+
+    }
+}
